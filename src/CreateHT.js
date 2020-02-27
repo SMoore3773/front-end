e25f157afb6a8f44c62a63af919c70e017125d91
SMoore3773 <Smooresculpture@gmail.com>
2020-02-26 21:48:02 -0500

    initial push and app setup

---
+++ src/CreateHT.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const CreateHT = (props) => {
+
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+export default CreateHT;
---
+++ src/Welcome.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Welcome = (props) =>{
+
+    return (
+        <div>
+            welcome
+        </div>
+    )
+}
+
+export default Welcome;
